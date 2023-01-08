191398f558af0ad6eb7a8c63cba8339438987cce
G3R74W <83832635+G3R74W@users.noreply.github.com>
2023-01-08 17:53:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+# for data transformation
+import numpy as np
+# for visualizing the data
+import matplotlib.pyplot as plt
+# for opening the media file
+import scipy.io.wavfile as wavfile
+import wave
+
+#reading wav file
+Fs, aud = wavfile.read('riff.wav')
+
+#openning wav file
+wav_obj = wave.open('riff.wav', 'rb')
+
+#sampling file
+sample_freq = wav_obj.getframerate()
+n_samples = wav_obj.getnframes()
+t_audio = n_samples/sample_freq
+
+# select left channel only
+aud = aud[:, 0]
+# trim the first 20 seconds
+first = aud[:int(Fs*20)]
+
+#amplitude en fct du temps
+times = np.linspace(0, n_samples/sample_freq, num=n_samples)
+plt.figure(figsize=(15, 5))
+ax1 = plt.subplot(211)
+plt.title("Visualisation du signal")
+plt.ylabel('Amplitude')
+plt.xlabel('Temps (s)')
+plt.xlim(0, t_audio)
+plt.plot(times, aud)
+plt.subplot(212, sharex=ax1)
+
+#spectrogramme de frequence
+powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(first, Fs=Fs)
+plt.xlabel("Temps (s)")
+plt.ylabel("Fréquence (Hz)")
+plt.title("Spectrogramme de fréquence")
+
+#add colorbar
+plt.colorbar()
+
+#add padding between figures
+plt.tight_layout(pad=2.0)
+plt.show()
+
+####################################################################
+#filtrage haute fréquence
+
+plt.specgram(first, Fs=Fs, vmin=20.0, vmax=21)
+plt.title('filtrage hautes fréquences')
+plt.xlabel('temps (s)')
+plt.ylabel('Fréquences (Hz)')
+plt.colorbar()
+plt.show()
+

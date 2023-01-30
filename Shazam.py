bec248b72fe0c808a42ff08882ae447bb21779d9
G3R74W <83832635+G3R74W@users.noreply.github.com>
2023-01-30 22:10:48 +0100

    Add files via upload

---
+++ Shazam.py
@@ -0,0 +1,106 @@
+# for data transformation
+import numpy as np
+# for visualizing the data
+import matplotlib.pyplot as plt
+# for opening the media file
+import scipy.io.wavfile as wavfile
+import wave
+from numpy.fft import fft, fftfreq
+
+#ce programme permet d'analyser un enregistrement audio et de le comparer a la base de donnees
+
+
+#il faut modifier a chaque execution le fichier audio a analyser
+#reading wav file
+Fs, aud = wavfile.read('Pompidou1970.wav')
+
+#openning wav file
+wav_obj = wave.open('Pompidou1970.wav', 'rb')
+
+#sampling file
+sample_freq = wav_obj.getframerate()
+n_samples = wav_obj.getnframes()
+t_audio = n_samples/sample_freq
+
+# select left channel only
+aud = aud[:, 0]
+# trim the first 30 seconds of the audio
+first = aud[:int(Fs*30)]
+
+times = np.linspace(0, n_samples/sample_freq, num=n_samples)
+
+powerSpectrum2, frequenciesFound2, time2, im = plt.specgram(first, Fs=Fs, vmin=20.0, vmax=21)
+
+#transfo de fourier
+# Calcul FFT
+X = fft(aud)  # Transformée de fourier
+freq = fftfreq(aud.size, d=1/Fs)  # Fréquences de la transformée de Fourier
+
+# Calcul du nombre d'échantillon
+N = aud.size
+
+# On prend la valeur absolue de l'amplitude uniquement pour les fréquences positives et normalisation
+X_abs = np.abs(X[:N//2])*2.0/N
+# On garde uniquement les fréquences positives
+freq_pos = freq[:N//2]
+
+points = plt.scatter(freq_pos, X_abs, s=10)
+
+#récupération des coordonées de chaque point de la TF
+coordinates = points.get_offsets()
+
+#filtrage de la liste pour avoir uniquement les coordonées des points du graph
+
+filtered_coordinates = [(x,y) for x,y in coordinates if x >= 0 and x <= 100 and y >= 10 and y <= 1000]
+
+next_file = False
+
+# Open the text file for reading
+with open('Mitterrand', 'r') as file:
+    # Read the contents of the file
+    file_contents = file.read().splitlines()
+    print(file_contents)
+    print(len(file_contents))
+    # Check if each element of the file contents is present in the compare list
+    for line in file_contents:
+        value = float(line.strip())
+
+        if value not in filtered_coordinates and next_file == False:
+            next_file = True
+            print("pas le bon")
+    if (next_file == False):
+        print("Il s'agit du président Mitterrand en 1994")
+
+
+if(next_file):
+    with open('Pompidou', 'r') as file:
+        # Read the contents of the file
+        file_contents = file.read().splitlines()
+        next_file = False
+
+        # Check if each element of the file contents is present in the compare list
+        for line in file_contents:
+            value = float(line.strip())
+
+            if value not in filtered_coordinates and next_file == False:
+                next_file = True
+                print("pas le bon")
+        if(next_file == False):
+            print("Il s'agit du président Pompidou en 1970")
+
+if(next_file):
+    with open('Sarkozy', 'r') as file:
+        # Read the contents of the file
+        file_contents = file.read().splitlines()
+        next_file = False
+
+
+        # Check if each element of the file contents is present in the compare list
+        for line in file_contents:
+            value = float(line.strip())
+
+            if value not in filtered_coordinates and next_file == False:
+                next_file = True
+                print("pas le bon")
+        if(next_file == False):
+            print("Il s'agit du président Sarkozy en 2008")
